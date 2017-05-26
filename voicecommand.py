098933a86e40d2122369517ab44e6cac661e9f68
ferdavid1 <fespinosa@ucdavis.edu>
2017-05-25 21:37:18 -0700

    updates

---
+++ voicecommand.py
@@ -0,0 +1,49 @@
+import speech_recognition as sr 
+import pyaudio
+import wave
+ 
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+CHUNK = 1024
+RECORD_SECONDS = 3
+WAVE_OUTPUT_FILENAME = "sound_file.wav"
+ 
+audio = pyaudio.PyAudio()
+ 
+# start Recording
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,
+                frames_per_buffer=CHUNK)
+print ("recording...")
+frames = []
+ 
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    frames.append(data)
+print ("finished recording")
+ 
+ 
+# stop Recording
+stream.stop_stream()
+stream.close()
+audio.terminate()
+ 
+waveFile = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(frames))
+waveFile.close()
+r = sr.Recognizer()
+with sr.AudioFile('sound_file.wav') as source:
+    audio = r.record(source) 
+    with open(r"cred.json", "r") as f:  
+        credentials_json = f.read()
+        try:
+            if r.recognize_google_cloud(audio, credentials_json=credentials_json, language='en-US') == 'tequila':
+                exec(open('servo.py').read())
+        except sr.UnknownValueError:
+            print("Google Cloud Speech could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Cloud Speech service; {0}".format(e))
